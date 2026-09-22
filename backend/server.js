12b43f51c56d5f9da9fa2a78839c70ecaace6324
EDUARDO LOPES DE CASTRO BARBOSA <eduardobarbosa0893@digitalcollege.internal>
2026-09-22 20:09:28 -0300

    add package-lock.json

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,28 @@
 import express from 'express'
-import mysql from 'mysql2'
+import mysql from 'mysql2/promise'
 import cors from 'cors'
 
-
-const port = 3000;
+const PORT = 3000;
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const conexao = mysql.createPool({
     host: "loja-db",
     user: "loja-app",
-    password: "",
-    database: "",
-})
+    password: "12345",
+    database: "loja",
+});
 
 app.get("/", (req, res)=>{
-    res.status(200).send("minha api está rodando")
+    res.status(200).send("minha api está rodando");
 });
 
-app.get("/produtos", (req, res)=>{
-    res.status(200).json()
+app.get("/produtos", async (req, res)=>{
+    const [produtos] = await conexao.query("SELECT * FROM produtos"); 
+    res.status(200).json(produtos);
 });
 
-app.listen(port, ()=>{
-    console.log(`api rodando em http://localhost:${port}`)
-});
-
+app.listen(PORT, ()=>{
+    console.log(`api rodando em http://localhost:${PORT}`);
+})
