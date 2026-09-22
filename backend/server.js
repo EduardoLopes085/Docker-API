96a9dfad2e0882708654d32686b2476b821cd9da
EDUARDO LOPES DE CASTRO BARBOSA <eduardobarbosa0893@digitalcollege.internal>
2026-09-22 19:12:57 -0300

    config mysql

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 import express from 'express'
+import mysql from 'mysql2'
+import cors from 'cors'
+
 
 const port = 3000;
 const app = express();
+app.use(express.json())
+app.use(cors())
+
+const conexao = mysql.createPool({
+    host: "loja-db",
+    user: "loja-app",
+    password: "",
+    database: "",
+})
 
 app.get("/", (req, res)=>{
     res.status(200).send("minha api está rodando")
 });
 
+app.get("/produtos", (req, res)=>{
+    res.status(200).json()
+});
+
 app.listen(port, ()=>{
-    console.log(``)
+    console.log(`api rodando em http://localhost:${port}`)
 });
 
